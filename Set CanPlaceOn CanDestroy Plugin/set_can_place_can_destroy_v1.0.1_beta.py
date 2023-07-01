a32e0dfb5e1e4176a5dad0f30e212d828a113b0e
StealthyX <stealthyexpertx@gmail.com>
2023-06-30 23:59:01 -0400

    set_can_place_can_destroy_v1.0.1_beta.py
    I'm excited to introduce the beta version of 1.0.1 which brings about significant speed enhancements, along with added functionality. Now, you can select specific items for adding NBT tags. In addition, the asterisk (*) key functions as an "All Items" key in the "Can Edit" settings list. It accepts item IDs with or without prefixes, thus supporting both Minecraft Vanilla and custom behavior packs.
    
    However, please note that support for Minecraft Java is still not available. This is due to the time required to update the plugin and understand how Minecraft Java Edition manages this particular type of NBT data. I appreciate your patience as I continue to work on these enhancements eventually!

--- Set CanPlaceOn CanDestroy Plugin/set_can_place_can_destroy_v1.0.1_beta.py
+++ Set CanPlaceOn CanDestroy Plugin/set_can_place_can_destroy_v1.0.1_beta.py
@@ -1,5 +1,6 @@
 #Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
-#By StealthyX - @RedstonerLabs - StealthyX#8940
+#By StealthyX - @RedstonerLabs - StealthyX#8940 or stealthyx or StealthyExpertX or StealthyExpert
+#If you want to use this Minecraft Marketplace commercially just reach out to me for permission!
 
 from amulet.api.selection import SelectionGroup
 from amulet.api.level import BaseLevel
@@ -56,6 +57,7 @@ from amulet_map_editor.programs.edit.api.operations import (
 
 canplaceon = "CanPlaceOn"
 candestroy = "CanDestroy"
+canedit = "CanEdit"
 plugin_title = str(canplaceon)+" / "+str(candestroy)+" Plugin"
 
 operation_options = {
@@ -74,9 +76,11 @@ operation_options = {
     ],
     "Block Ids for "+str(canplaceon)+":": ["str", "grass, dirt, stone"],
     "Block Ids for "+str(candestroy)+":": ["str", "grass, dirt, stone"],
+    "Edit Ids:": ["str", "*"],
 }
 
-def _get_vanilla_block(dim, world, x, y, z, trans):
+def _get_vanilla_block(dim, world, be_coordinates, trans):
+    (x, y, z) = be_coordinates 
     #barrowed from the amulet api. #thanks gentlegiant.
     cx, cz = block_coords_to_chunk_coords(x, z)
     offset_x, offset_z = x - 16 * cx, z - 16 * cz
@@ -98,6 +102,20 @@ def parse_list_of_blocks(string_of_blocks):
             raise ValueError("[ERROR!] Use this format grass, dirt, stone")
     else:
         return [False, None]
+
+def parse_list_of_edits(string_of_edits):
+    # Check if string_of_edits is empty or contains only whitespace
+    if not string_of_edits or string_of_edits.isspace():
+        return ["*"]
+
+    # Split the string into words, considering space, comma, and semicolon as separators
+    can_edits = re.split(r'[;\s,]+', string_of_edits)
+
+    # Ensure that the blocks list is not empty after the split
+    if can_edits:
+        return can_edits
+    else:
+        return ["*"]
 
 def add_nbttags(all_blocks, has_custom_block, block_dict_use):
     blocks_idsX = ListTag()
@@ -125,10 +143,33 @@ def get_block_lists(world, platform, version):
     base_names = version.block.base_names("minecraft", True)
     return base_names
 
+def is_valid_id(item_name_id_str, id_list):
+    # Check if item_name_id_str contains '*'
+    if '*' in item_name_id_str:
+        return True
+
+    # Split the item_name_id_str by ':'
+    parts = item_name_id_str.split(':')
+
+    # Check if item_name_id_str is valid
+    # It should either have no ':' or exactly one ':'
+    # Also, each part of the item_name_id_str should not be empty
+    if len(parts) > 2 or '' in parts:
+        return False
+
+    # Check if each character in item_name_id_str is alphanumeric, underscore, or ':'
+    for char in item_name_id_str:
+        if not (char.isalnum() or char in ['_', ':']):
+            return False
+
+    # Check if item_name_id_str is in id_list
+    if item_name_id_str not in id_list:
+        return False
+
+    return True
+
 def template_function(world: BaseLevel, dimension: Dimension, selection: SelectionGroup, options: dict):
     chunk_coordinates = selection.chunk_locations()
-
-    world = world
     platform = world.level_wrapper.platform
     world_version = world.level_wrapper.version
 
@@ -138,13 +179,10 @@ def template_function(world: BaseLevel, dimension: Dimension, selection: Selecti
     can_destroy_check = options[candestroy+":"]
     raw_can_place_list = options["Block Ids for "+canplaceon+":"]
     raw_can_destroy_list = options["Block Ids for "+candestroy+":"]
+    raw_can_edit_list = options["Edit Ids:"]
 
-    blocks_can_place = parse_list_of_blocks(raw_can_place_list)[1]
-    blocks_can_destroy = parse_list_of_blocks(raw_can_destroy_list)[1]
-
-    blocks_can_place_check = parse_list_of_blocks(raw_can_place_list)[0]
-    blocks_can_destroy_check = parse_list_of_blocks(raw_can_destroy_list)[0]
-
+    blocks_can_place_check, blocks_can_place = parse_list_of_blocks(raw_can_place_list)
+    blocks_can_destroy_check, blocks_can_destroy = parse_list_of_blocks(raw_can_destroy_list)
 
     block_dict_check = {
         canplaceon: blocks_can_place_check,
@@ -156,40 +194,42 @@ def template_function(world: BaseLevel, dimension: Dimension, selection: Selecti
         candestroy: blocks_can_destroy
     }
 
+    can_edits_list = parse_list_of_edits(raw_can_edit_list)
+
     trans = world.translation_manager.get_version(platform, world_version)
     mc_version = (platform, world_version)
 
+    print ("Started editing...")
     for chunk_coords in chunk_coordinates:
         cx, cz = chunk_coords
-        print ("Started editing...")
 
         try:
             chunk = world.get_chunk(cx, cz, dimension)
-            block_entities = chunk.block_entities
-            be_keys = chunk.block_entities
+            be_keys = set(chunk.block_entities.keys())
 
-            for be_coordinates in be_keys.keys():
-                if be_coordinates in selection: 
+            for be_coordinates in be_keys & selection:
                     (x, y, z) = be_coordinates 
-                    (block, block_entity) = _get_vanilla_block(dimension, world, x, y, z, trans)
+                    (block, block_entity) = _get_vanilla_block(dimension, world, be_coordinates, trans)
 
-                    if block_entity is not None:
-                        if "Items" in block_entity.nbt:
+                    if block_entity is not None and "Items" in block_entity.nbt:
                             #loops over each item in the container.
-                            for ind, item in enumerate(block_entity.nbt["Items"]):
-                                if can_place_on_check:
-                                    bk_list1 = add_nbttags(all_blocks, block_dict_check.get(canplaceon), block_dict_use.get(canplaceon))
-                                    block_entity.nbt["Items"][ind][canplaceon] = bk_list1
+                            for item_nbt in block_entity.nbt["Items"]:
+                                item_name_id_str = item_nbt["Name"]
 
-                                if can_destroy_check:
-                                    bk_list2 = add_nbttags(all_blocks, block_dict_check.get(candestroy), block_dict_use.get(candestroy))
-                                    block_entity.nbt["Items"][ind][candestroy] = bk_list2
+                                if is_valid_id(item_name_id_str, can_edits_list):
+                                    if can_place_on_check:
+                                        bk_list1 = add_nbttags(all_blocks, block_dict_check.get(canplaceon), block_dict_use.get(canplaceon))
+                                        item_nbt[canplaceon] = bk_list1
+    
+                                    if can_destroy_check:
+                                        bk_list2 = add_nbttags(all_blocks, block_dict_check.get(candestroy), block_dict_use.get(candestroy))
+                                        item_nbt[candestroy] = bk_list2
 
                             world.set_version_block(x, y, z, dimension, mc_version, block, block_entity)
 
         except ChunkLoadError:
             print(f'Chunk error at {chunk_coords}')
-        print ("Completed editing!")
+    print ("Completed editing!")
 
 export = {
     "name": plugin_title,
