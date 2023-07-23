85000df35756001dc09f86c62cf88015dfc42f58
StealthyExpert <stealthyexpertx@gmail.com>
2023-07-23 03:47:20 -0400

    Improved Speed, Error Handling, and Better Azalea Leaf Handling
    Enhanced Error Handling, Code Professionalism, and Performance; Added Commenting and Special Handling for Azalea Leaves

--- Fix Leaf Decay Plugin/fix_leaf_decay_plugin.py
+++ Fix Leaf Decay Plugin/fix_leaf_decay_plugin.py
@@ -27,6 +27,7 @@ Your feedback is invaluable! I'm open to updating this plugin based on popular d
 
 #standard library imports
 import time
+import traceback
 from typing import Tuple, Any 
 
 #third-party imports
@@ -41,6 +42,7 @@ from amulet.api.selection import SelectionGroup
 from amulet.utils.world_utils import block_coords_to_chunk_coords
 from amulet_nbt import ByteTag, StringTag
 
+#dictionary mapping each operation option to its UI representation and default state.
 operation_options = {
 	"Prevents leaf blocks in your world from decaying.\nChoose the types of leaves you want to keep,\nrun the plugin, and they'll be set to stay persistent!": ["label"],
 	"TREE TYPES": ["label"],
@@ -56,6 +58,7 @@ operation_options = {
 	"Cherry": ["bool", True]
 }
 
+#mapping for converting leaf block state properties between Minecraft Java and Bedrock editions.
 leaves_universal_mapping = {
 	"java": {
 		"1.20.0": {
@@ -143,13 +146,15 @@ leaves_universal_mapping = {
 	}
 }
 
-def _get_from_dict(data_dict, map_list):
+#retrieves nested values from a dictionary using a list of keys.
+def get_from_dict(data_dict, map_list):
 	for k in map_list:
 		data_dict = data_dict.get(k, {})
 	return data_dict
 
+#determines whether a bedrock block requires an old or new leaf type.
 def get_leaf_info(platform, key_version, leaf):
-	leaf_info = _get_from_dict(leaves_universal_mapping, [platform, key_version, leaf])
+	leaf_info = get_from_dict(leaves_universal_mapping, [platform, key_version, leaf])
 	blockstate, properties = leaf_info if leaf_info else [None, {}]
 
 	for key in ['old_leaf_type', 'new_leaf_type']:
@@ -158,6 +163,7 @@ def get_leaf_info(platform, key_version, leaf):
 
 	return None
 
+#formats nanoseconds into a human-readable string.
 def pretty_time_delta_ns(nanoseconds):
 	sign_string = '-' if nanoseconds < 0 else ''
 	nanoseconds = abs(int(nanoseconds))
@@ -180,6 +186,7 @@ def pretty_time_delta_ns(nanoseconds):
 	else:
 		return f"{sign_string}{nanoseconds}ns"
 
+#fetches a block and its extras at given coordinates from a world and translates them from universal format.
 def get_block(dimension, world, x, y, z, world_versions) -> Tuple[Any, Any, Any]:
 	world_trans = world.translation_manager.get_version(*world_versions)
 
@@ -190,10 +197,10 @@ def get_block(dimension, world, x, y, z, world_versions) -> Tuple[Any, Any, Any]
 	chunk = world.get_chunk(chunk_coords[0], chunk_coords[1], dimension)
 	runtime_id = chunk.blocks[cx, y, cz]
 	uni_block = chunk.block_palette[runtime_id]
-	uni_tile_entity = chunk.block_entities.get((x, y, z), None)
+	(block_object, _, block_extras) = world_trans.block.from_universal(uni_block, None)
+	return (block_object, block_extras)
 
-	return world_trans.block.from_universal(uni_block, uni_tile_entity)
-
+#determines the nearest applicable version for a given key from a mapping, considering world's platform and version.
 def get_closest_version(world,leaves_universal_mapping, world_platform, world_version, key):
 	version_number = world.translation_manager.get_version(world_platform, world_version).version_number
 
@@ -211,6 +218,7 @@ def get_closest_version(world,leaves_universal_mapping, world_platform, world_ve
 	key_dict = platform_dict.get(closest_version, {}).get(key, {})
 	return [key,key_dict,closest_version]
 
+#retrieves the types of trees for a specific game version.
 def get_trees_in_version(world,leaves_universal_mapping, world_platform, world_version):
 	version_number = world.translation_manager.get_version(world_platform, world_version).version_number
 
@@ -228,7 +236,8 @@ def get_trees_in_version(world,leaves_universal_mapping, world_platform, world_v
 	tree_keys = platform_dict.get(closest_version, {}).keys()
 	return tree_keys
 
-def set_state_of_block(world, x, y, z, dimension, world_versions, block_namespace, block_name, block_prop, block_extras):
+#places a leaf block at given coordinates with provided properties and extra blocks.
+def set_leaf_block(world, x, y, z, dimension, world_versions, block_namespace, block_name, block_prop, block_extras):
 	leaves_block = Block(
 		block_namespace,
 		block_name,
@@ -237,24 +246,34 @@ def set_state_of_block(world, x, y, z, dimension, world_versions, block_namespac
 	)
 	world.set_version_block(x, y, z, dimension, world_versions, leaves_block)
 
+#fixes leaf decay issues in Minecraft world based on user's selection and world's version/platform.
 def fix_leaf_decay(world: BaseLevel, dimension: Dimension, selection: SelectionGroup, options: dict):
 	try:
+		#starts the operation timer and prints a starting message
 		start_time = time.perf_counter_ns()
 		print("\n" * 2 + "Starting the operation...")
 
+		#copies the selected options into a new variable
 		tree_bool = options.copy()
 
+		#extracts platform and version information from the world
 		world_platform = world.level_wrapper.platform
 		world_version = world.level_wrapper.version
 		world_versions = (world_platform, world_version)
+		
+		#maps appropriate leaf properties keys based on the world platform
 		persistent_key = {"java":"persistent","bedrock":"persistent_bit"}.get(world_platform)
 		bedrock_update_bit_key = "update_bit"
+
+		#predefines NBT tags for future use
 		byte_tag_true = ByteTag(1)
 		byte_tag_false = ByteTag(0)
 		string_tag_true = StringTag("true")
 
+		#retrieves tree types for the current world version and platform
 		tree_types = get_trees_in_version(world,leaves_universal_mapping, world_platform, world_version)
 
+		#maps selected tree types to the closest available version in the world instance
 		trees = []
 		found_version = False
 		for treebool in tree_bool.keys():
@@ -265,57 +284,79 @@ def fix_leaf_decay(world: BaseLevel, dimension: Dimension, selection: SelectionG
 					print("Loaded leaves mapping from version: "+str(key_version))
 					found_version = True
 
-		if len(trees) < 1:
+		#throws error if no tree types were selected
+		if not trees:
 			raise ValueError("You need to select atleast one checkbox!")
 
+		#iterates over each block in the selection
 		for box in selection.merge_boxes().selection_boxes:
 			for x, y, z in box:
-				(block_object, block_entity, block_extras) = get_block(dimension, world, x, y, z, world_versions)
+				#retrieves block and associated data at given coordinates
+				(block_object, block_extras) = get_block(dimension, world, x, y, z, world_versions)
 
+				#extracts block name details
 				block_name = block_object.base_name
 				block_namespace = block_object.namespace
 				block_namespaced = block_object.namespaced_name
 
-				if "leave" not in block_name or block_namespace not in "minecraft":
+				#skips non-leaf or non-vanilla blocks
+				if "leave" not in block_name or block_namespace != "minecraft":
 					continue
 
+				#iterates over selected tree types
 				for valid_tree in trees:
 					key_dicts = valid_tree[1]
 
+					#checks if current block matches with the selected tree type
 					if key_dicts[0] == block_namespaced:
 						treekey = valid_tree[0]
 						key_version = valid_tree[2]
 						block_prop = block_object.properties
 
+						#retrieves leaf type information for the current version
 						leaves_type = get_leaf_info(world_platform, key_version, treekey)
 
+						#handles specific properties for Bedrock edition leaf blocks
 						if world_platform == "bedrock":
+
+							#updates block properties to prevent decay
+							should_break = False
+
 							if leaves_type is not None:
 								leaf_type_value, leaf_type_key = leaves_type
-								leaf_block = block_prop[leaf_type_value]
+								leaf_block = block_prop.get(leaf_type_value, None)
 
-								if leaf_block == leaf_type_key:
-									block_prop[persistent_key] = byte_tag_true
-									block_prop[bedrock_update_bit_key] = byte_tag_false
-									set_state_of_block(world, x, y, z, dimension, world_versions, block_namespace, block_name, block_prop, block_extras)
+								if leaf_block is not None and leaf_block == leaf_type_key:
+									should_break = True
 
-							else:
-								block_prop[persistent_key] = byte_tag_true
-								block_prop[bedrock_update_bit_key] = byte_tag_false
-								set_state_of_block(world, x, y, z, dimension, world_versions, block_namespace, block_name, block_prop, block_extras)
+							block_prop[persistent_key] = byte_tag_true
+							block_prop[bedrock_update_bit_key] = byte_tag_false
+							set_leaf_block(world, x, y, z, dimension, world_versions, block_namespace, block_name, block_prop, block_extras)
 
+							if should_break:
+								break
+
+						#handles Java Edition leaf blocks; updates the block properties to prevent decay
 						elif world_platform == "java":
 							block_prop[persistent_key] = string_tag_true
-							set_state_of_block(world, x, y, z, dimension, world_versions, block_namespace, block_name, block_prop, block_extras)
+							set_leaf_block(world, x, y, z, dimension, world_versions, block_namespace, block_name, block_prop, block_extras)
+							break
 
+	#handles exceptions and prints out detailed error information for debugging
 	except Exception as e:
 		print()
-		print(f"ERROR MESSAGE: {str(e)}")
+		print("ERROR MESSAGE:")
+		print(str(e))
+		print()
+		print("TRACEBACK:")
+		print(traceback.format_exc())
 
 	else:
+		#calculates and prints the elapsed time once the operation is successfully completed
 		elapsed_time = time.perf_counter_ns() - start_time
 		print(f"Operation completion time: {pretty_time_delta_ns(elapsed_time)}")
 
+#exports the operation and its options for use in the program
 export = {
 	"name": "Fix Leaf Decay",
 	"operation": fix_leaf_decay,
